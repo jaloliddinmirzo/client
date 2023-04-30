afbaea2ae83577b9e00e3e4b7d7b85fa01e5b228
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-05-01 03:55:05 +0500

    Saytni cliet tomoni yaratilmoqda

---
+++ pages/About.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <Head>
+        <title>About</title>
+    </Head>
+    <div>About</div>
+    L
+    </>
+  )
+}
+
+export default About
